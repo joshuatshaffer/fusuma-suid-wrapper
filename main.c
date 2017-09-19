4eafedd35a4f852672192e408f90139af88380b8
joshuatshaffer <joshuatshaffer@gmail.com>
2017-09-19 17:04:25 -0500

    Now finding gid from group name instead of hard coding it.

--- main.c
+++ main.c
@@ -4,9 +4,24 @@
 #include <sys/types.h>
 #include <sys/fsuid.h>
 #include <grp.h>
+#include <errno.h>
 #include <unistd.h>
 
-void addgroup (gid_t group_to_add) {
+gid_t get_gid (char *name) {
+    errno = 0;
+    struct group *ginfo = getgrnam(name);
+    if (ginfo == NULL) {
+        if (errno == 0) {
+            fprintf(stderr, "The group \"%s\" could not be found.\n", name);
+            exit (1);
+        } else {
+            perror ("Could not get group info");
+        }
+    }
+    return ginfo->gr_gid;
+}
+
+void join_group (gid_t group_to_add) {
     int num_groups = getgroups (0, NULL);
     gid_t *list = calloc (num_groups + 1, sizeof(gid_t));
     getgroups (num_groups, list + 1);
@@ -17,14 +32,13 @@ void addgroup (gid_t group_to_add) {
 void drop_sudo () {
     uid_t uid = getuid();
     seteuid (uid);
-    setfsuid (uid);
 }
 
 void exec_pass_args (char *file, int argc, char **argv) {
     // Make a copy of argv including the NULL terminator.
     char **argv_new = calloc (argc + 1, sizeof(char**)); 
     memcpy (argv_new, argv, (argc + 1) * sizeof(char**));
-    // Set the program name.
+    // Set the program name arg.
     argv_new[0] = file;
     execvp (file, argv_new);
     perror ("execvp returned");
@@ -32,9 +46,14 @@ void exec_pass_args (char *file, int argc, char **argv) {
 
 int main (int argc, char *argv[]) {
 
-    // Add input to our groups.
-    addgroup (106);
+    gid_t input_gid = get_gid ("input");
 
+    // Add input to our groups.
+    join_group (input_gid);
+
+    // This in needed to fix "Insecure operation - spawn (SecurityError)"
+    // which was being thrown by read_libinput in fusuma.
+    // I don't know why this is needed.
     drop_sudo();
 
     // Exec Fusuma passing though command line args.
