c943a4556c9c1c48b6c4e143940fb267f23c39d5
joshuatshaffer <joshuatshaffer@gmail.com>
2017-09-21 14:29:05 -0500

    Cleaned up code and improved makefile.

--- main.c
+++ main.c
@@ -3,14 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/fsuid.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 gid_t get_gid (char *name) {
     errno = 0;
-    struct group *ginfo = getgrnam (name);
-    if (ginfo == NULL) {
+    struct group *group_info = getgrnam (name);
+    if (group_info == NULL) {
         if (errno == 0) {
             fprintf (stderr, "The group \"%s\" could not be found.\n", name);
             exit (1);
@@ -18,15 +16,19 @@ gid_t get_gid (char *name) {
             perror ("Could not get group info");
         }
     }
-    return ginfo->gr_gid;
+    return group_info->gr_gid;
 }
 
 void join_group (gid_t group_to_add) {
+    // Get the groups we are currently in.
     int num_groups = getgroups (0, NULL);
-    gid_t *list = calloc (num_groups + 1, sizeof (gid_t));
-    getgroups (num_groups, list + 1);
-    list[0] = group_to_add;
-    setgroups (num_groups + 1, list);
+    gid_t *groups = calloc (num_groups + 1, sizeof (gid_t));
+    getgroups (num_groups, groups + 1);
+
+    // Add the new group.
+    groups[0] = group_to_add;
+
+    setgroups (num_groups + 1, groups);
 }
 
 void drop_sudo () {
@@ -38,24 +40,23 @@ void exec_pass_args (char *file, int argc, char **argv) {
     // Make a copy of argv including the NULL terminator.
     char **argv_new = calloc (argc + 1, sizeof (char **));
     memcpy (argv_new, argv, (argc + 1) * sizeof (char **));
+
     // Set the program name arg.
     argv_new[0] = file;
+
     execvp (file, argv_new);
     perror ("execvp returned");
 }
 
 int main (int argc, char *argv[]) {
-
-    gid_t input_gid = get_gid ("input");
-
     // Add input to our groups.
+    gid_t input_gid = get_gid ("input");
     join_group (input_gid);
 
-    // This in needed to fix "Insecure operation - spawn (SecurityError)"
-    // which was being thrown by read_libinput in fusuma.
-    // I don't know why this is needed.
+    // Drop sudo privaleges.
+    // They are no longer needed and should not be passed on to fusuma.
     drop_sudo ();
 
-    // Exec Fusuma passing though command line args.
+    // Exec Fusuma and pass along the command line args.
     exec_pass_args ("fusuma", argc, argv);
 }
--- makefile
+++ makefile
@@ -1,9 +1,20 @@
 
+DESTDIR=/usr/local/bin
+
 all: fusuma-s
 
 fusuma-s: main.c
-	gcc main.c -o fusuma-s
+	gcc -std=gnu11 -pedantic -Wall -Wextra main.c -o fusuma-s
 
-rootup: fusuma-s
-	sudo chown root:root fusuma-s
-	sudo chmod u+s fusuma-s
+clean:
+	rm -rf fusuma-s
+
+install: fusuma-s
+	cp fusuma-s fusuma-s-dup
+	chown root:root fusuma-s-dup \
+	&& chmod u+s fusuma-s-dup \
+	&& mv fusuma-s-dup $(DESTDIR)/fusuma-s \
+	|| rm fusuma-s-dup # Clean up this temp file if the above fails.
+
+uninstall:
+	rm $(DESTDIR)/fusuma-s
